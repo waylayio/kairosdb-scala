be550bbf0e836c392f26db942dce7169d642f9e2
Thomas Toye <toye.thomas@gmail.com>
2016-08-12 12:45:53 +0200

    Add test for unparseable JSON

--- src/test/scala/unit/QuerySpec.scala
+++ src/test/scala/unit/QuerySpec.scala
@@ -3,7 +3,7 @@ package unit
 import java.time.Instant
 
 import io.waylay.kairosdb.driver.KairosDB
-import io.waylay.kairosdb.driver.KairosDB.{KairosDBResponseBadRequestException, KairosDBResponseInternalServerErrorException}
+import io.waylay.kairosdb.driver.KairosDB.{KairosDBResponseBadRequestException, KairosDBResponseInternalServerErrorException, KairosDBResponseParseException}
 import io.waylay.kairosdb.driver.models.Aggregator.{Average, Sum}
 import io.waylay.kairosdb.driver.models.GroupBy.{GroupByTags, GroupByType}
 import io.waylay.kairosdb.driver.models.KairosCompatibleType.KNumber
@@ -162,6 +162,90 @@ class QuerySpec extends Specification {
       ))
 
       val r = kairosDb.queryMetrics(qm) must be_==(expected).await(1, 3.seconds)
+      mockWs.close()
+      r
+    }
+
+    "return a failed future if KairosDB sends unparseable JSON" in { implicit ee: ExecutionEnv =>
+      val mockWs = MockWS {
+        case ("POST", "http://localhost:8080/api/v1/datapoints/query") => Action { req =>
+          val expected = Json.parse(
+            """
+              |{
+              |   "start_absolute": 1357023600000,
+              |   "end_relative": {
+              |       "value": "5",
+              |       "unit": "days"
+              |   },
+              |   "time_zone": "Asia/Kabul",
+              |   "metrics": [
+              |       {
+              |           "tags": {
+              |               "host": ["foo", "foo2"],
+              |               "customer": ["bar"]
+              |           },
+              |           "name": "abc.123",
+              |           "limit": 10000,
+              |           "aggregators": [
+              |               {
+              |                   "name": "sum",
+              |                   "sampling": {
+              |                       "value": "10",
+              |                       "unit": "minutes"
+              |                   }
+              |               }
+              |           ]
+              |       },
+              |       {
+              |           "tags": {
+              |               "host": ["foo", "foo2"],
+              |               "customer": ["bar"]
+              |           },
+              |           "name": "xyz.123",
+              |           "aggregators": [
+              |               {
+              |                   "name": "avg",
+              |                   "sampling": {
+              |                       "value": "10",
+              |                       "unit": "minutes"
+              |                   }
+              |               }
+              |           ]
+              |       }
+              |   ]
+              |}
+            """.stripMargin)
+          val response = Json.parse(
+            """
+              |{}
+            """.stripMargin)
+
+          req.body.asJson.map(x => if(x == expected) Ok(response) else BadRequest) getOrElse BadRequest
+        }
+      }
+
+      val kairosDb = new KairosDB(mockWs, KairosDBConfig(), global)
+
+      val query1 = Query(
+        MetricName("abc.123"),
+        Seq(QueryTag("host", Seq("foo", "foo2")), QueryTag("customer", Seq("bar"))),
+        aggregators = Seq(Sum(10.minutes)),
+        limit = Some(10000)
+      )
+
+      val query2 = Query(
+        MetricName("xyz.123"),
+        Seq(QueryTag("host", Seq("foo", "foo2")), QueryTag("customer", Seq("bar"))),
+        aggregators = Seq(Average(10.minutes))
+      )
+
+      val qm = QueryMetrics(
+        Seq(query1, query2),
+        TimeSpan(AbsoluteStartTime(Instant.ofEpochMilli(1357023600000L)), Some(RelativeEndTime(5.days))),
+        timeZone = Some("Asia/Kabul")
+      )
+
+      val r = kairosDb.queryMetrics(qm) must throwA[KairosDBResponseParseException].await(1, 3.seconds)
       mockWs.close()
       r
     }
